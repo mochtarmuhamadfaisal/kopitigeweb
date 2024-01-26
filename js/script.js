78107a738148c7ea1e053a3f5346bd58220e0380
mochtarmuhammadfaisal <mochtarmuhammadfaisal@gmail.com>
2024-01-26 16:38:41 +0700

    search form ui

--- js/script.js
+++ js/script.js
@@ -1,15 +1,29 @@
-// toggle class active
+// toggle class active untuk hamburger menu
 const navbarNav = document.querySelector('.navbar-nav');
 // ketika hamburgernya menu di klik
 document.querySelector('#hamburger-menu').onclick = () => {
     navbarNav.classList.toggle('active');
 }
 
-// jika klik diluar sidebar untuk menghilangkan nav
-const hamburger = document.querySelector('#hamburger-menu');
+// jika klik diluar elemen
+const hm = document.querySelector('#hamburger-menu');
+const sb = document.querySelector('#search-button');
 
 document.addEventListener('click', function (e) {
-    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
         navbarNav.classList.remove('active');
     }
+    if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+        searchForm.classList.remove('active');
+    }
 })
+
+// toggle class active untuk search form
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+// ketika hamburgernya menu di klik
+document.querySelector('#search-button').onclick = (e) => {
+    searchForm.classList.toggle('active');
+    searchBox.focus();
+    e.preventDefault();
+}
