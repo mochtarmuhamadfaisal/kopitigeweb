fc56664001b3d3e4b54a6304f4469fdd933f9a85
mochtarmuhammadfaisal <mochtarmuhammadfaisal@gmail.com>
2024-01-29 11:18:46 +0700

    ui cart detail + modal detail produk

--- js/script.js
+++ js/script.js
@@ -6,14 +6,15 @@ document.querySelector('#hamburger-menu').onclick = () => {
     navbarNav.classList.toggle('active');
 }
 
-document.querySelector('#shopping-cart-button').onclick = () => {
+document.querySelector('#shopping-cart-button').onclick = (e) => {
     shoppingCartButton.classList.toggle('active');
+    e.preventDefault();
 }
 
 // jika klik diluar elemen
 const hm = document.querySelector('#hamburger-menu');
 const sb = document.querySelector('#search-button');
-const scb = document.querySelector('#shopping-cart-button');
+const sc = document.querySelector('#shopping-cart-button');
 
 document.addEventListener('click', function (e) {
     if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
@@ -22,7 +23,7 @@ document.addEventListener('click', function (e) {
     if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
         searchForm.classList.remove('active');
     }
-    if (!scb.contains(e.target) && !shoppingCartButton.contains(e.target)) {
+    if (!sc.contains(e.target) && !shoppingCartButton.contains(e.target)) {
         shoppingCartButton.classList.remove('active');
     }
 })
@@ -35,4 +36,30 @@ document.querySelector('#search-button').onclick = (e) => {
     searchForm.classList.toggle('active');
     searchBox.focus();
     e.preventDefault();
+}
+
+
+// ketik klik icon mata dan muncul modal
+const itemDetailModal = document.querySelector('#item-detail-modal');
+const itemDetailButtons = document.querySelectorAll('#item-detail-button');
+
+// ketika item button di klik
+itemDetailButtons.forEach((btn) => {
+    btn.onclick = (e) => {
+        itemDetailModal.style.display = 'flex';
+        e.preventDefault();
+    }
+});
+
+// ketika klik tombol close
+document.querySelector('.modal .close-icon').onclick = (e) => {
+    itemDetailModal.style.display = "none";
+    e.preventDefault();
+}
+
+// ketika klik di luar modal
+window.onclick = (e) => {
+    if (e.target === itemDetailModal) {
+        itemDetailModal.style.display = 'none';
+    }
 }
